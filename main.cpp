3d9c47db58a9a0be72a888983e2e34b9ba88d4a8
lcplcplcp111 <961261579@qq.com>
2022-05-15 16:27:13 +0800

    二次提交

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, name!" << std::endl;
+    std::cout << "Hello, name!" << std::endl;
 //    int i=0;
 //    int count=0;
 //    for (i=0;i<5;i++)
