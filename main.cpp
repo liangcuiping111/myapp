d5cfd4905fc5a9bbbc199154188483d1a638a61c
liangcuiping111 <105704972+liangcuiping111@users.noreply.github.com>
2022-05-17 15:36:33 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, name!" << std::endl;
     std::cout << "Hello, name!" << std::endl;
+    std::cout << "Hello, name! HaHaHaHa!@!!" << std::endl;
     int i=0;
     int count=0;
     for (i=0;i<5;i++)
