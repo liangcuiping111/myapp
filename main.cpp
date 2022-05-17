112ccf898a5d4352f338bb7d25133bca5718028e
lcplcplcp111 <961261579@qq.com>
2022-05-17 10:25:11 +0800

    四次提交

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, name!" << std::endl;
     std::cout << "Hello, name!" << std::endl;
-//    int i=0;
-//    int count=0;
-//    for (i=0;i<5;i++)
-//    {
-//        count+=i;
-//    }
-//    std::cout << "Hello, World!！" << std::endl;
-//    std::cout << "Hello, World!！！！" << std::endl;
+    int i=0;
+    int count=0;
+    for (i=0;i<5;i++)
+    {
+        count+=i;
+    }
+    std::cout << "Hello, World!！" << std::endl;
+    std::cout << "Hello, World!！！！" << std::endl;
     return 0;
 }
